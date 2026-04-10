8d8639b9621ded1cc0210475592c82b443147cc7
Codewithvanshh <omksah008@gmail.com>
2026-04-10 17:44:53 +0530

    Add files via upload

---
+++ WORKING CALCULATOR .py
@@ -0,0 +1,17 @@
+# CALCULATOR
+ 
+a = float(input("Enter your first number: "))
+b = float(input("Enter your second number: "))
+
+addition = a+b
+multiplication = a*b
+substraction = a-b
+division = a/b
+average = (a+b)/2
+
+
+print("The addition of your numbers is: ",addition)
+print("The multiplication of your numbers is: ",multiplication)
+print("the substraction of your numbers is: ",substraction)
+print("the division of your number is: ",division)
+print("The average of your number is: ",average)
